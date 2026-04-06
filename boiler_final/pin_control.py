c90d7740b7dd0088ced24fb94c9d207da88899f1
Annankun <124212659+Annankun@users.noreply.github.com>
2026-04-06 01:45:57 -0400

    Delete boiler_final directory

--- boiler_final/pin_control.py
+++
@@ -1,8 +0,0 @@
-import RPi.GPIO as GPIO
-
-def toggle_pin(pin):
-	current_state = GPIO.input(pin)
-	if current_state == GPIO.HIGH:
-		GPIO.output(pin, GPIO.LOW)
-	else:
-		GPIO.output(pin, GPIO.HIGH)
