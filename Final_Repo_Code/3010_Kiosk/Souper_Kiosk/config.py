8810e5c79b6a4c1e2c7e2104d138661f26558181
Annankun <124212659+Annankun@users.noreply.github.com>
2026-04-06 01:38:22 -0400

    Delete Final_Repo_Code/3010_Kiosk directory

--- Final_Repo_Code/3010_Kiosk/Souper_Kiosk/config.py
+++
@@ -1,7 +0,0 @@
-FIREBASE_CONFIG = {
-    "apiKey": "AIzaSyAC0Ln-uwvVyqcfXWOO52_uFt-q_KuVr9A",
-    "authDomain": "soupercomputer-f0dad.firebaseapp.com",
-    "databaseURL": "https://soupercomputer-f0dad-default-rtdb.firebaseio.com",
-    "projectId": "soupercomputer-f0dad",
-    "storageBucket": "soupercomputer-f0dad.firebasestorage.app"
-}
